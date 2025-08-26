c7ef2a6f216c568298d3f6463d07b6ee1ef4df09
AntonCapusta25 <131923021+AntonCapusta25@users.noreply.github.com>
2025-08-26 23:21:42 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-
+import { AuthProvider } from './contexts/AuthContext'
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -11,6 +11,7 @@ import Calendar from "./pages/Calendar";
 import Search from "./pages/Search";
 import Settings from "./pages/Settings";
 import NotFound from "./pages/NotFound";
+
 
 const queryClient = new QueryClient();
 
