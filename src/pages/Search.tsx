92457a010fd8d9f0dcee5fb2c621f1620b48f1db
AntonCapusta25 <131923021+AntonCapusta25@users.noreply.github.com>
2025-08-26 22:10:30 +0200

    Create Search.tsx

---
+++ src/pages/Search.tsx
@@ -0,0 +1,13 @@
+
+import { Layout } from '../components/Layout';
+import { SearchResults } from '../components/SearchResults';
+
+const Search = () => {
+  return (
+    <Layout>
+      <SearchResults />
+    </Layout>
+  );
+};
+
+export default Search;
