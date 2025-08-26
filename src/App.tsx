86e4cf943f81b2a0593e43a4218d511cb78f983a
AntonCapusta25 <131923021+AntonCapusta25@users.noreply.github.com>
2025-08-26 22:13:25 +0200

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
+import MyTasks from "./pages/MyTasks";
+import Team from "./pages/Team";
+import Calendar from "./pages/Calendar";
+import Search from "./pages/Search";
+import Settings from "./pages/Settings";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/my-tasks" element={<MyTasks />} />
+          <Route path="/team" element={<Team />} />
+          <Route path="/calendar" element={<Calendar />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/settings" element={<Settings />} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
