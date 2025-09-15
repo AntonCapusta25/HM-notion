64c8ec9be43ad201ef7ebd99e6229325f0af58bc
AntonCapusta25 <131923021+AntonCapusta25@users.noreply.github.com>
2025-09-16 00:08:23 +0200

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,14 +1,13 @@
 import { createClient } from '@supabase/supabase-js'
 
+
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error('Supabase URL and/or Anon Key are missing from environment variables.')
 }
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
-  auth: {
-    multiTab: false, // <-- This is the fix
-  },
-})
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
